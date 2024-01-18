084f54b8333a06c751544ba380ca569dbb0de666
ravidasari0789 <ravidasari0789@gmail.com>
2024-01-18 17:18:53 +0530

    enquirecontroller commit for add requirement for store data stundent enquire

--- src/main/java/com/dataforge/controller/EnquiryController.java
+++ src/main/java/com/dataforge/controller/EnquiryController.java
@@ -1,15 +1,21 @@
 package com.dataforge.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.dataforge.entity.StudentEnqInfo;
 import com.dataforge.model.SearchCriteria;
 import com.dataforge.service.EnquiryServiceImpl;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EnquiryController {
@@ -21,22 +27,35 @@ public class EnquiryController {
 		
 		model.addAttribute("studentenq", new StudentEnqInfo());
 		
-		return "enqpage";
+		return "studentadd";
 	}
 	@PostMapping("/handleenquire")
-	public String addEnquiry(StudentEnqInfo se, Model model) {
-		boolean s=enquiryServiceImpl.addEnq(se);
-		model.addAttribute("enquire", s);
-		  return "enqpage";
+	public String addEnquiry(@ModelAttribute("studentenq") StudentEnqInfo studentenq,HttpServletRequest request, Model model) {
+		HttpSession session=request.getSession(false);
+		Integer id=(Integer) session.getAttribute("CID");
+		studentenq.setCid(id);
+		boolean s=enquiryServiceImpl.addEnq(studentenq);
+		if(s) {
+		model.addAttribute("enqsucess", "Saved Sucessfully Student Enquire");
+		}else {
+			model.addAttribute("enqerror","Failed to saved Student Enquire");
+		}
+		  return "studentadd";
 	}
 
-	public String viewEnquiries(Model model) {
+	@GetMapping("/allviewpage")
+	public String viewEnquiries(HttpServletRequest req, Model model) {
 		
+		HttpSession session=req.getSession(false);
+		Integer id=(Integer) session.getAttribute("CID");
+	List<StudentEnqInfo> list=	enquiryServiceImpl.getEnquiries(id, new SearchCriteria());
+		model.addAttribute("student", list);
 		return "viewenquire";
 	}
-
-	public String filterEnquiries(SearchCriteria sc, Model model) {
-		
-		return "filterenquires";  
+    @PostMapping("/filter")
+	public String filterEnquiries(SearchCriteria sc, Model model) {	
+    List<StudentEnqInfo> list=	enquiryServiceImpl.getEnquiries(null, sc);
+    	model.addAttribute("entities", list);
+		return "viewenquire";  
 	}
 }
